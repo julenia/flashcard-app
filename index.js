c9ba9c8fac6f84a9b399852526dc0681b75ae546
julenia <julia.jankowska@onet.eu>
2019-07-05 13:42:35 +0200

    after pressing the button new flashcatf appear

--- index.js
+++ index.js
@@ -1,58 +1,57 @@
-function showAnswer(){
+function showAnswer() {
     console.log('hello');
-    document.getElementById('card').innerHTML=null;
+    document.getElementById('card').innerHTML = null;
     const ans = document.createElement('h1');
     card.appendChild(ans);
-
-}
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
-
-
-
-const data = {
-    cards : [
-        {
-            question: "Netherlands",
-            answer: "Amsterdam",
-        },
-        {
-            question:"Brussels",
-            answer: "Belgium",
-        },
-        {
-            question: "France",
-            answer: "Paris",
-        },
-        {
-            question: "Poland",
-            answer: "Warsaw",
-        },
-        {
-            question: "Ukraine",
-            answer: "Kiev",
-        },
-    ]
-}
-const random = getRndInteger(0, data.cards.length);
-for(let i=0; i<data.cards.length; i++){
-    if(i==random){ 
-    const card = document.getElementById('card');
-    const h1 = document.createElement('h1');
-    const question = data.cards[i].question;
-    console.log(question);
-    h1.innerHTML=question;
-    card.appendChild(h1);
-    const cards = data.cards[i];
-    card.onclick = function showAnswer(){
-        card.innerHTML=null;
-        const ans = document.createElement('h1');
-        const answer = data.cards[i].answer;
-        ans.innerHTML=answer;
-        card.appendChild(ans);
-    
-    }
-}
-
  }
+ function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+ }
+ const data = {
+    cards: [{
+        question: "Netherlands",
+        answer: "Amsterdam",
+    }, {
+        question: "Brussels",
+        answer: "Belgium",
+    }, {
+        question: "France",
+        answer: "Paris",
+    }, {
+        question: "Poland",
+        answer: "Warsaw",
+    }, {
+        question: "Ukraine",
+        answer: "Kiev",
+    }, ]
+ }
+ function giveQuestion(currentData) {
+    const random = getRndInteger(0, currentData.cards.length);
+    for (let i = 0; i < currentData.cards.length; i++) {
+        if (i == random) {
+            const card = document.getElementById('card');
+            card.innerHTML = null;
+            const h1 = document.createElement('h1');
+            const question = currentData.cards[i].question;
+            console.log(question);
+            h1.innerHTML = question;
+            card.appendChild(h1);
+            const cards = currentData.cards[i];
+            card.onclick = function showAnswer() {
+                card.innerHTML = null;
+                const ans = document.createElement('h1');
+                const answer = currentData.cards[i].answer;
+                ans.innerHTML = answer;
+                card.appendChild(ans);
+            }
+        }
+    }
+ }
+ giveQuestion(data)
+ const button = document.querySelector('button');
+ console.log(button);
+ button.onclick = function(){
+    giveQuestion(data);
+}
+ 
+ 
