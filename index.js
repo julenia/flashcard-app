045d1a66fc5bcc4827990a44afc043562d9a662c
julenia <julia.jankowska@onet.eu>
2019-07-05 10:37:46 +0200

    create index files and onclick listener

---
+++ index.js
@@ -0,0 +1,32 @@
+const card = document.getElementById('card');
+card.onclick = function showAnswer(){
+    console.log('hello');
+
+}
+
+
+
+const data = {
+    cards : [
+        {
+            question: "Netherlands",
+            answer: "Amsterdam",
+        },
+        {
+            question:"Brussels",
+            answer: "Belgium",
+        },
+        {
+            question: "France",
+            answer: "Paris",
+        },
+        {
+            question: "Poland",
+            answer: "Warsaw",
+        },
+        {
+            question: "Ukraine",
+            answer: "Kiev",
+        },
+    ]
+}
